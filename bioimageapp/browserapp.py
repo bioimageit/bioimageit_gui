2e0e31931af112931f0f83f85572bbcbbd6dc32f
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2020-06-11 19:06:50 +0200

    create experiment button now working

--- bioimageapp/browserapp.py
+++ bioimageapp/browserapp.py
@@ -13,9 +13,10 @@ from bioimageapp.browser.states import BiBrowserStates
 from bioimageapp.browser.containers import BiBrowserContainer
 from bioimageapp.browser.components import BiBrowserComponent
 
-from bioimageapp.experiment.states import BiExperimentStates
-from bioimageapp.experiment.containers import BiExperimentContainer
-from bioimageapp.experiment.components import BiExperimentComponent
+from bioimageapp.experiment.states import BiExperimentStates, BiExperimentCreateStates
+from bioimageapp.experiment.containers import BiExperimentContainer, BiExperimentCreateContainer
+from bioimageapp.experiment.components import BiExperimentComponent, BiExperimentCreateComponent
+from bioimageapp.experiment.models import BiExperimentCreateModel
                                             
 
 class BiBrowserApp(BiComponent):
@@ -24,12 +25,18 @@ class BiBrowserApp(BiComponent):
 
         # container
         self.browserContainer = BiBrowserContainer()
+        self.experimentCreateContainer = BiExperimentCreateContainer()
         
         # components
         self.browserComponent = BiBrowserComponent(self.browserContainer)
+        self.experimentCreateComponent = BiExperimentCreateComponent(self.experimentCreateContainer)
+
+        # models
+        self.expreimentCreateModel = BiExperimentCreateModel(self.experimentCreateContainer)
 
         # connections
         self.browserContainer.register(self)
+        self.experimentCreateContainer.register(self)
 
         # load settings
         self.browserContainer.currentPath = str(Path.home())
@@ -69,7 +76,18 @@ class BiBrowserApp(BiComponent):
 
         if action.state == BiExperimentStates.Loaded:
             self.tabWidget.setTabText(self.tabWidget.currentIndex(), action.parent_container.experiment.metadata.name)
-                
+
+        if action.state == BiBrowserStates.NewExperimentClicked:
+            self.experimentCreateComponent.get_widget().setVisible(True)
+
+        if action.state == BiExperimentCreateStates.CancelClicked:
+            self.experimentCreateComponent.get_widget().setVisible(False)  
+
+        if action.state == BiExperimentCreateStates.ExperimentCreated:
+            self.browserContainer.openExperimentPath = self.experimentCreateContainer.experiment_dir
+            self.browserContainer.emit(BiBrowserStates.OpenExperiment)
+            self.experimentCreateComponent.get_widget().setVisible(False)     
+
     def get_widget(self):
         return self.widget 
      
