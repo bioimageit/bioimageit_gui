bacb3c3a8ccb859f75e5f9d20147f2bbdadecc06
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2020-06-12 18:10:50 +0200

    fix bug in runnerapp config

--- runnerapp.py
+++ runnerapp.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import QApplication
@@ -17,18 +18,21 @@ if __name__ == '__main__':
     process_xml = ""
     if len(sys.argv) > 1:
         process_xml = sys.argv[1]
-    #if process_xml == "":
-    #    process_xml = "../bioimagepy/tests/test_processes_local/svdeconv/svdeconv2D.xml"
     if process_xml == "":
         raise CommandArgsError("No processs XML file")
 
-    
     # Create and show the component
-    ConfigAccess('config.sample.json') 
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path_parent = os.path.abspath(os.path.join(dir_path, os.pardir))
+    ConfigAccess(os.path.join(dir_path_parent,'config.json')) 
     component = BiRunnerApp(process_xml)
+    #rec = QApplication.desktop().screenGeometry()
+    #component.get_widget().resize(rec.width()/2, rec.height()/2) 
     component.get_widget().show()
     
     # Run the main Qt loop
-    app.setStyleSheet("file:///" + "theme/dark/stylesheet.css")
-    app.setWindowIcon(QIcon("theme/default/icon.png"))
+    stylesheet_path = os.path.join(dir_path, 'theme', 'dark', 'stylesheet.css')
+    app.setStyleSheet("file:///" + stylesheet_path)
+    icon_path = os.path.join(dir_path, "theme", "default", "icon.png" )
+    app.setWindowIcon(QIcon(icon_path))
     sys.exit(app.exec_())
