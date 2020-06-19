432c2793091f14a26607f30fba55704a2e0adf92
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2020-06-19 17:39:35 +0200

    add config file for package script

---
+++ config.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import json
+from pathlib import Path
+
+def write_json(metadata: dict, md_uri: str):
+    """Write the metadata to the a json file"""
+    with open(md_uri, 'w') as outfile:
+        json.dump(metadata, outfile, indent=4)  
+
+if __name__ == '__main__':
+    
+    # create the bookmarks file at bookmarks.json
+    bioimageapp_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+    package_dir = Path(bioimageapp_dir).parent
+
+    config_json = dict()  
+    config_json["bookmarks"] = []
+    userdata = {"name": "userdata", "url": os.path.join(package_dir, "userdata")}
+    config_json["bookmarks"].append(userdata)
+
+    config_file = os.path.join(package_dir, 'bookmarks.json')
+    write_json(config_json, config_file)
