f0857e7fa5f63981eaa28899b2ab3bf5ac8d7745
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2021-11-24 18:11:29 +0100

    update updater

--- bioimageit_gui/settings/_models.py
+++ bioimageit_gui/settings/_models.py
@@ -42,6 +42,10 @@ class BiUpdateThread(QThread):
         conda_dir = ConfigAccess.instance().get('runner')['conda_dir']
 
         script = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'update.sh')
+        p1 = subprocess.run(['chmod', '+x', script], shell=True, capture_output=True)
+        print( 'exit status:', p1.returncode )
+        print( 'stdout:', p1.stdout.decode() )
+        print( 'stderr:', p1.stderr.decode() )
         p = subprocess.run([script, install_dir, conda_dir], shell=True, capture_output=True)           
         print( 'exit status:', p.returncode )
         print( 'stdout:', p.stdout.decode() )
